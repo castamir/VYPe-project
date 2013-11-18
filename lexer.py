f7cc44d8096d07da83396c6b86adb8b0858cce18
castamir <miras.paulik@email.cz>
2013-11-18 18:02:25 +0100

    lexer now scans allmost every possible lexem, except keywords

--- lexer.py
+++ lexer.py
@@ -1,31 +1,92 @@
 import sys
-sys.path.insert(0,"./libs/ply")
 
-tokens = (
-    'NAME','NUMBER',
-    )
+sys.path.insert(0, "./libs")
 
-literals = ['=','+','-','*','/', '(',')']
+# Reserved words
+tokens = [
+    # Literals (identifier, indentifier type, integer constant, string constant, char const)
+    'ID', 'TYPEID', 'INTEGER', 'STRING', 'CHARACTER',
 
-# Tokens
+    # Operators (+, -, *, /, %, ||, &&, !, <, <=, >, >=, ==, !=)
+    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MODULO',
+    'OR', 'AND', 'NOT',
+    'LT', 'LE', 'GT', 'GE', 'EQ', 'NE',
 
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    # Assignment (=)
+    'EQUALS',
 
-def t_NUMBER(t):
+    # Ternary operator (?)
+    'TERNARY',
+
+    # Delimeters ( ) , . ; :
+    'LPAREN', 'RPAREN',
+    'COMMA', 'PERIOD', 'SEMI', 'COLON',
+]
+
+# Operators
+t_PLUS             = r'\+'
+t_MINUS            = r'-'
+t_TIMES            = r'\*'
+t_DIVIDE           = r'/'
+t_MODULO           = r'%'
+t_OR              = r'\|\|'
+t_AND             = r'&&'
+t_NOT             = r'!'
+t_LT               = r'<'
+t_GT               = r'>'
+t_LE               = r'<='
+t_GE               = r'>='
+t_EQ               = r'=='
+t_NE               = r'!='
+
+# Assignment operators
+t_EQUALS           = r'='
+
+# ?
+t_TERNARY          = r'\?'
+
+# Delimeters
+t_LPAREN           = r'\('
+t_RPAREN           = r'\)'
+t_COMMA            = r','
+t_PERIOD           = r'\.'
+t_SEMI             = r';'
+t_COLON            = r':'
+
+# Identifiers TODO - reserved strings and keywords
+t_ID = r'[A-Za-z_][A-Za-z0-9_]*'
+
+# Integer literal
+def t_INTEGER(t):
     r'\d+'
-    t.value = int(t.value)
+    t.value = (int) (t.value)
     return t
 
-t_ignore = " \t"
+# String literal
+t_STRING = r'\"([^\\\n]|(\\.))*?\"'
 
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count("\n")
-    
+# Character constant 'c' or L'c'
+t_CHARACTER = r'(L)?\'([^\\\n]|(\\.))*?\''
+
+# Comment (C-Style)
+def t_COMMENT(t):
+    r'/\*(.|\n)*?\*/'
+    t.lexer.lineno += t.value.count('\n')
+    t.lexer.skip(1)
+    return t
+
+# Comment (C++-Style)
+def t_BLOCKCOMMENT(t):
+    r'//.*\n'
+    t.lexer.lineno += 1
+    t.lexer.skip(1)
+    return t
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-    
+
 # Build the lexer
 import ply.lex as lex
+
 lex.lex()
