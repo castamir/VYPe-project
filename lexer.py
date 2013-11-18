a7fcb1f61d479f01c36335577770e52021ad3bba
castamir <miras.paulik@email.cz>
2013-11-18 15:02:14 +0100

    project init from PLY calc.py example

---
+++ lexer.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.insert(0,"./libs")
+import lexer
+
+tokens = (
+    'NAME','NUMBER',
+    )
+
+literals = ['=','+','-','*','/', '(',')']
+
+# Tokens
+
+t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+    
+# Build the lexer
+import ply.lex as lex
+lex.lex()
